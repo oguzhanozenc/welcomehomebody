7f846904664c57aa6cf034210e1e3578fb8e06a1
Oğuzhan Özenç <oguzhanozenc10@gmail.com>
2024-09-11 00:50:37 +0300

    addition of links to corresponding product item in the cart and checkout stages for easy item redirects to the product

--- src/components/CheckoutPage.jsx
+++ src/components/CheckoutPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useShopifyCart } from "../hooks/useShopifyCart";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import "../styles/CheckoutPage.css";
 
 const CheckoutPage = () => {
@@ -33,26 +33,38 @@ const CheckoutPage = () => {
     <div className="checkout-container">
       <h2>Checkout</h2>
       <ul className="checkout-items">
-        {cartItems.map((item) => (
-          <li key={item.variant.id} className="checkout-item">
-            <div className="checkout-item-image">
-              <img src={item.product.images[0]} alt={item.product.title} />
-            </div>
-            <div className="checkout-item-details">
-              <h3>{item.product.title}</h3>
-              <p>{item.product.description}</p>
-              <p>Price: ${item.variant.priceV2.amount}</p>
-              <p>Quantity: {item.quantity}</p>
-              <button
-                onClick={() => handleRemoveFromCart(item.variant.id)}
-                disabled={loading || processing}
-                className="remove-button"
-              >
-                Remove
-              </button>
-            </div>
-          </li>
-        ))}
+        {cartItems.map((item) => {
+          // Remove the gid://shopify/Product/ prefix to get only the product ID
+          const productId = item.product.id.replace(
+            "gid://shopify/Product/",
+            ""
+          );
+
+          return (
+            <li key={item.variant.id} className="checkout-item">
+              <div className="checkout-item-image">
+                <Link to={`/products/${productId}`}>
+                  <img src={item.product.images[0]} alt={item.product.title} />
+                </Link>
+              </div>
+              <div className="checkout-item-details">
+                <Link to={`/products/${productId}`}>
+                  <h3>{item.product.title}</h3>
+                </Link>
+                <p>{item.product.description}</p>
+                <p>Price: ${item.variant.priceV2.amount}</p>
+                <p>Quantity: {item.quantity}</p>
+                <button
+                  onClick={() => handleRemoveFromCart(item.variant.id)}
+                  disabled={loading || processing}
+                  className="remove-button"
+                >
+                  Remove
+                </button>
+              </div>
+            </li>
+          );
+        })}
       </ul>
       <div className="checkout-actions">
         <button onClick={handleBackToCart} className="checkout-button">
